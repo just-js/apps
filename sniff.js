e5e532a026254ef522de4c7c9d5bef04891d9e64
Andrew Johnston <apjohnsto@gmail.com>
2021-10-21 04:18:26 +0100

    tweaks

--- sniff.js
+++ sniff.js
@@ -1,19 +1,18 @@
 const binary = require('@binary')
 const { Parser, protocols } = require('@packet')
+const { sys } = just.library('sys')
+const { hrtime } = sys
 
-let last = just.hrtime()
+let last = hrtime()
 
 function onPacket (packet, u8) {
-  const time = just.hrtime()
+  const time = hrtime()
   const { offset, bytes, frame, header } = packet
-  just.print(time - last)
   if (frame && frame.protocol === 'IPv4' && header && header.protocol === protocols.TCP) {
-    // tcp frames
-    just.print(tcpDump(packet))
+    just.print(`\n${tcpDump(packet)}`)
     if (bytes > offset) just.print(dump(u8.slice(offset, bytes)), false)
   } else if (frame && frame.protocol === 'IPv4' && header && header.protocol === protocols.UDP) {
-    // handle a udp message
-    just.print(udpDump(packet))
+    just.print(`\n${udpDump(packet)}`)
     if (bytes > offset) just.print(dump(u8.slice(offset, bytes)), false)
   }
   last = time
@@ -30,7 +29,6 @@ function main (args) {
   const fd = net.socket(PF_PACKET, SOCK_RAW, htons16(ETH_P_ALL))
   if (fd < 0) throw new SystemError('socket')
   if (iff) {
-    // bind to a specific interface
     const mac = new ArrayBuffer(6)
     let r = net.getMacAddress(fd, iff, mac)
     if (r < 0) throw new SystemError('getMacAddress')
@@ -39,11 +37,9 @@ function main (args) {
     just.print(`bound to interface ${iff} (${toMAC(new Uint8Array(mac))})`)
   }
   while (1) {
-    // this is synchronous - no need for the event loop
     const bytes = net.recv(fd, buf, 0, buf.byteLength)
     if (bytes === 0) break
     if (bytes < 0) throw new SystemError('recv')
-    // hack to ignore duplicates on lo until we have recvfrom: https://stackoverflow.com/questions/17194844/packetsocket-opened-on-loopback-device-receives-all-the-packets-twice-how-to-fi
     if (!(iff === 'lo' && ((i++ % 2) === 0))) onPacket(parse(bytes, true), u8)
   }
   net.close(fd)
